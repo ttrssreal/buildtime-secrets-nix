f73fbe9e5a3227d22115130d4b1275ce99a2a3b0
Jess <jess@jessie.cafe>
2025-10-18 04:54:33 +1300

    ci: init

--- libnixstore/setup-tests.sh
+++ libnixstore/setup-tests.sh
@@ -1,6 +1,6 @@
 #/usr/bin/env nix shell
 #!nix nixpkgs#nix
 
-nix build \
-  github:nixos/nixpkgs/e9f00bd893984bc8ce46c895c3bf7cac95331127#dwm-status \
-  --print-out-paths
+nix eval \
+  github:nixos/nixpkgs/e9f00bd893984bc8ce46c895c3bf7cac95331127#dwm-status
+  >/dev/null
